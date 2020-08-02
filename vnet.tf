d760a0b80086c90fa55982a976f7a7e2fc8497e5
Ela Molina <elamercedes@hotmail.com>
2020-08-02 21:19:42 +0000

    fix main

--- vnet.tf
+++ vnet.tf
@@ -1,5 +1,12 @@
 module "network" {
   source  = "app.terraform.io/ELITA-training/network/azurerm"
   version = "3.1.1"
-  prefix = "elita"
+
+  address_space = "11.0.0.0/16"
+  dns_servers = "test"
+  resource_group_name = "ela2-vnet"
+  subnet_names = "ela2-subnet"
+  subnet_prefixes = "11.0.10.0/24"
+  tags = "test"
+  vnet_name = "ela2-vnet"
 }

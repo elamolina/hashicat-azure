d2f8353ffc8689f44184ccc511f8e1ad2cd9dd59
elamolina <47539671+elamolina@users.noreply.github.com>
2020-08-03 21:25:13 -0500

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/ELITA-training/network/azurerm"
   version = "3.1.2"
 
-  resource_group_name = "elita-workshop"
+  resource_group_name = "elita2-workshop"
 }
